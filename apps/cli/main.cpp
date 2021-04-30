926334e825a4dea964fa44dcbff530859d7de3f6
Michel Estermann <estermann.michel@gmail.com>
2021-04-30 17:11:37 +0200

    Refactored cli app to use fmt.

--- apps/cli/main.cpp
+++ apps/cli/main.cpp
@@ -2,8 +2,9 @@
 #include "coronan/http_client.hpp"
 #include "coronan/ssl_initializer.hpp"
 
-#include <iostream>
+#include <fmt/core.h>
 #include <lyra/lyra.hpp>
+#include <sstream>
 
 int main(int argc, char* argv[])
 {
@@ -26,7 +27,6 @@ int main(int argc, char* argv[])
       nullptr, certificate_handler_ptr, context_ptr);
 
 */
-
   static auto const ssl_initializer_handler =
       coronan::SSLInitializer::initialize_with_accept_certificate_handler();
   try
@@ -36,10 +36,14 @@ int main(int argc, char* argv[])
     auto cli = lyra::cli_parser() | lyra::help(help_request) |
                lyra::opt(country, "country")["-c"]["--country"]("Country Code");
 
+    std::stringstream usage;
+    usage << cli;
+
     auto const result = cli.parse({argc, argv});
     if (!result)
     {
-      std::cerr << "Error in comman line: " << result.errorMessage() << "\n";
+      fmt::print(stderr, "Error in comman line: {}\n", result.errorMessage());
+      fmt::print("{}\n", usage.str());
       // Poco::Net::uninitializeSSL();
       // Clean Code Note: Was forgotten
       exit(EXIT_FAILURE);
@@ -47,40 +51,48 @@ int main(int argc, char* argv[])
 
     if (help_request)
     {
-      std::cout << cli;
+      fmt::print("{}\n", usage.str());
       // Poco::Net::uninitializeSSL();
       // Clean Code Note: Was forgotten
       exit(EXIT_SUCCESS);
     }
 
     // Clean Code Note: With auto the typ must not be written explicitly
-    auto const url = "https://corona-api.com/countries/" + country;
+    auto const url = std::string{"https://corona-api.com/countries/"} + country;
 
-    auto response = coronan::HTTPClient::get(url);
+    auto const response = coronan::HTTPClient::get(url);
 
-    auto const& data =
-        coronan::api_parser::parse_country(response.get_response_body());
-    std::cout << "\"datetime\", \"confirmed\", \"death\", \"recovered\", "
-                 "\"active\"\n";
-
-    // Clean Code Note: Range based for loop is much less verbose and clearer to
-    // understand
-
-    auto const to_string = [](auto val) {
-      return val.has_value() ? std::to_string(val.value()) : "--";
-    };
-
-    for (auto const& data_point : data.timeline)
+    if (response.get_status() == Poco::Net::HTTPResponse::HTTP_OK)
     {
-      std::cout << data_point.date << ", " << to_string(data_point.confirmed)
-                << ", " << to_string(data_point.deaths) << ", "
-                << to_string(data_point.recovered) << ", "
-                << to_string(data_point.active) << "\n";
+
+      auto const& data =
+          coronan::api_parser::parse_country(response.get_response_body());
+      fmt::print("\"datetime\", \"confirmed\", \"death\", \"recovered\", "
+                 "\"active\"\n");
+
+      // Clean Code Note: Range based for loop is much less verbose and clearer
+      // to understand
+      for (auto const& data_point : data.timeline)
+      {
+        fmt::print("{date}, {confirmed}, {deaths}, {recovered}, {active}\n",
+                   fmt::arg("date", data_point.date),
+                   fmt::arg("confirmed", data_point.confirmed.value_or(0)),
+                   fmt::arg("deaths", data_point.deaths.value_or(0)),
+                   fmt::arg("recovered", data_point.recovered.value_or(0)),
+                   fmt::arg("active", data_point.active.value_or(0)));
+      }
+    }
+    else
+    {
+      fmt::print("Error fetching {url}. Response Status: {status}.\n",
+                 fmt::arg("url", url),
+                 fmt::arg("status", response.get_status()));
     }
   }
   catch (coronan::SSLException const& ex)
   {
-    std::cerr << "SSL Exception: " << ex.displayText() << "\n";
+
+    fmt::print(stderr, "SSL Exception: {}\n", ex.displayText());
     exit(EXIT_FAILURE);
   }
   // Poco::Net::uninitializeSSL(); called in destructor of the static
